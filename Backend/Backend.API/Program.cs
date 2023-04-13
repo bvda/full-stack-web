5eacce5d03cb5248d0c3231fd539acd337657a45
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2023-04-13 13:55:43 +0200

    Add models, services, and controllers

--- Backend/Backend.API/Program.cs
+++ Backend/Backend.API/Program.cs
@@ -1,7 +1,12 @@
+using Backend.API.Data;
+using Backend.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<CardContext>();
+builder.Services.AddTransient<CardService>();
+builder.Services.AddTransient<SetService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,8 +20,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
